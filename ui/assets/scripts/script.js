2491ded8ed02111c6578ededa53220d436033c9d
narrowdesign <nick@narrowdesign.com>
2017-05-15 22:33:41 -0400

    done

--- ui/assets/scripts/script.js
+++ ui/assets/scripts/script.js
@@ -1,3 +1,5 @@
+var spiraling = false;
+var rotation = 0;
 $(function() {
 
   var WIN = $(window);
@@ -6,29 +8,68 @@ $(function() {
 
   var _winW;
   var _winH;
+  var smallScreen;
   var aspect = .618033;
   var axis = .7237;
   var spiralOrigin;
   var colorsArray = [];
   var bgColorsArray = [];
-  var rotation = 0;
   var currentSection = 0;
   var currentSectionName;
   var touchStartY = 0;
+  var touchStartX = 0;
   var moved = 0;
   var animRAF;
   var animating = false;
   var isTouchDevice;
   var showLineTimeout;
+  var accessible = false;
 
-  var safari = false;
-  if (navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1) { safari = true; }
+  var mobileSafari;
+
+  var standalone = window.navigator.standalone,
+      userAgent = window.navigator.userAgent.toLowerCase(),
+      safari = userAgent.indexOf('safari') != -1 && userAgent.indexOf('chrome') == -1,
+      firefox = userAgent.indexOf('firefox') != -1 || userAgent.indexOf('mozilla') == -1,
+      ios = /iphone|ipod|ipad/.test( userAgent );
+
+  if (firefox) {
+    BODY.addClass('is-firefox');
+  }
+  if( ios ) {
+      safari = true;
+      if ( !standalone && safari ) {
+          mobileSafari = true;
+
+      } else if ( standalone && !safari ) {
+          // STANDALONE
+
+      } else if ( !standalone && !safari ) {
+          // UIWEBVIEW
+
+      };
+  }
 
   WIN.on('touchstart',function(){
     isTouchDevice = true;
   })
 
   resizeHandler();
+
+  if (!smallScreen) {
+    $('.js-section-title-word').each(function(i){
+      var title = $('.js-section-title-word').eq(i);
+      title.html(function(index, html) {
+        html = html.replace(/&amp;/g, '&');
+        return html.replace(/\S/g, '<span>$&</span>');
+      });
+      $('span',title).each(function(j){
+        $('span',title).eq(j).css({
+          transitionDelay: j/20 + 's'
+        })
+      })
+    });
+  }
 // EVENTS
 /////////
 
@@ -36,37 +77,53 @@ $(function() {
   WIN.on('resize',resizeHandler);
 
   WIN.on('wheel', function(e) {
-    moved = e.originalEvent.deltaY || 0;
-    rotation += moved/-10;
-    e.preventDefault();
-    startScrollTimeout()
-    cancelAnimationFrame(animRAF);
-    requestAnimationFrame(function(){
-      scrollHandler();
-    })
+    if (!accessible) {
+      moved = e.originalEvent.deltaY || 0;
+      rotation += moved/-10;
+      rotation = trimRotation();
+      if (!BODY.hasClass('is-gallery')) {
+        e.preventDefault();
+        startScrollTimeout()
+        cancelAnimationFrame(animRAF);
+        scrollHandler();
+      }
+    }
   });
 
   WIN.on('touchstart', function(e) {
-    var touch = e.originalEvent.touches[0] || e.originalEvent.changedTouches[0];
-    touchStartY = touch.pageY;
-    cancelAnimationFrame(animRAF);
+    if (!accessible) {
+      var touch = e.originalEvent.touches[0] || e.originalEvent.changedTouches[0];
+      moved = 0;
+      touchStartX = touch.pageX;
+      touchStartY = touch.pageY;
+      cancelAnimationFrame(animRAF);
+    }
   })
   WIN.on('touchmove', function(e) {
-    var touch = e.originalEvent.touches[0] || e.originalEvent.changedTouches[0];
-    moved = (touchStartY - touch.pageY) * 3;
-    touchStartY = touch.pageY;
-    rotation += moved/-10;
-    startScrollTimeout();
-    cancelAnimationFrame(animRAF);
-    scrollHandler()
+    if (!accessible) {
+      if (!BODY.hasClass('is-gallery')) {
+        e.preventDefault()
+        var touch = e.originalEvent.touches[0] || e.originalEvent.changedTouches[0];
+        moved = ((touchStartY - touch.pageY)+(touchStartX - touch.pageX)) * 3;
+        touchStartX = touch.pageX;
+        touchStartY = touch.pageY;
+        rotation += moved/-10;
+        rotation = trimRotation();
+        startScrollTimeout();
+        cancelAnimationFrame(animRAF);
+        scrollHandler()
+      }
+    }
   });
   WIN.on('touchend', function(e) {
-    animateScroll()
+    if (!accessible) {
+      animateScroll()
+    }
   })
   WIN.on('scroll',function(e) {
 
   })
-  WIN.on('mousemove',moveCloseIcon)
+  WIN.on('mousemove',mousemoveHandler)
 
   WIN.on('keydown',function(e) {
     if (e.keyCode === 39 || e.keyCode === 40 || e.keyCode === 32) {
@@ -79,17 +136,59 @@ $(function() {
     scrollHandler()
   })
 
+  $('#canvas').on('click',function(){
+    clearTimeout(showLineTimeout)
+    animateScroll(0,rotation)
+  })
+
+  $('.js-skip').on('click',function(){
+    if (!accessible) {
+      accessible = "somewhat";
+      BODY.addClass('is-more-accessible')
+      $('.js-accessible').text('less')
+    } else {
+      accessible = false;
+      $('.js-accessible').text('more')
+      BODY.addClass('is-less-accessible')
+      resizeHandler()
+    }
+  })
+
+  $('.js-scroll').on('click',function(e){
+    animateScroll((currentSection + 1) * -90,rotation)
+    e.stopPropagation()
+  })
   $('.js-section').on('click',function() {
     cancelAnimationFrame(animRAF)
     animateScroll($(this).index() * -90,rotation);
   })
 
-  $('.js-view-icon').on('click', function(e) {
-    BODY.addClass('is-gallery');
-    moveCloseIcon(e);
-    for (var i=1;i<=4;i++) {
-      $('.js-gallery img').eq(i-1).attr('src','ui/assets/images/projects/' + currentSectionName + i + '.jpg')
+  $('.js-gallery-description-box').on('click',function(e) {
+    e.stopPropagation()
+  })
+
+  $('.js-project-cta').on('click', function(e) {
+    if ($(this).parent().parent().hasClass('active')) {
+      e.stopPropagation()
+      document.body.scrollTop = 0;
+      BODY.addClass('is-gallery');
+      $('.js-gallery-description').html($('.js-section.active .js-description').html())
+      $('.js-gallery-title').html($('.js-section.active .js-section-title').html())
+      moveCloseIcon(e);
+      $('.js-gallery img').attr('src','')
+      for (var i=1;i<=4;i++) {
+        $('.js-gallery img').eq(i-1).attr('src','ui/assets/images/projects/' + currentSectionName + i + '.jpg')
+      }
     }
+  })
+  $('.js-project-cta').on('mouseenter',function(){
+    $('.js-view-text', this).text('Open')
+  })
+  $('.js-project-cta').on('mouseleave',function(){
+    $('.js-view-text', this).text('View')
+  })
+  $('.js-gallery').on('click', function() {
+    BODY.removeClass('is-gallery');
   })
   $('.js-close-icon').on('click', function() {
     BODY.removeClass('is-gallery');
@@ -98,93 +197,185 @@ $(function() {
 // FUNCTIONS
 ////////////
   function animateScroll(targR,startR,speed) {
-    var distance = startR - targR;
-    var mySpeed = speed || .2;
-    if ((targR && Math.abs(targR - rotation) > .1) || Math.abs(moved) > 1) {
-      if (targR) {
-        rotation += mySpeed * (targR - rotation);
-      } else {
-        moved *= .93;
-        rotation += moved/-10;
+    if (!accessible) {
+      var distance = startR - targR;
+      var mySpeed = speed || .2;
+      if (((targR || Math.abs(targR) === 0) && Math.abs(targR - rotation) > .1) || Math.abs(moved) > 1) {
+        if (targR || Math.abs(targR) === 0) {
+          rotation += mySpeed * (targR - rotation);
+        } else {
+          moved *= .98;
+          rotation += moved/-10;
+        }
+        rotation = trimRotation();
+        scrollHandler();
+        animRAF = requestAnimationFrame(function(){
+          animateScroll(targR,startR,speed)
+        });
+      } else if (targR || Math.abs(targR) === 0) {
+        cancelAnimationFrame(animRAF)
+        rotation = targR;
+        rotation = trimRotation();
+        scrollHandler();
       }
-      scrollHandler();
-      animRAF = requestAnimationFrame(function(){
-        animateScroll(targR,startR,speed)
-      });
-    } else if (targR) {
-      cancelAnimationFrame(animRAF)
-      rotation = targR;
-      scrollHandler();
     }
   }
   function scrollHandler() {
-    var scale = Math.pow(aspect,rotation/90);
-    $('.js-spiral').css({
-      transform: 'rotate(' + rotation + 'deg) scale(' + scale + ')',
-    })
-    currentSection = Math.floor((rotation-30)/-90);
-    currentSectionName = $('.js-section').eq(currentSection).data('project')
-    var bg = bgColorsArray[currentSection-1]
-    var color = colorsArray[currentSection-1]
-    BODY.css({
-      backgroundColor: bg,
-      color: color
-    })
-    $('.js-section').css({
-      color: color
-    })
-    $('.js-view-icon').css({
-      background: color,
-      color: bg
-    })
-    $('.js-view-icon svg rect').css({
-      stroke: bg
-    })
-    $('.js-nav').css({
-      color: color
-    })
-    $('.js-nav svg path').css({
-      fill: color
-    })
-    for (var i=0;i<$('.js-section').length;i++) {
-      if (i - currentSection < -1) {
-        $('.js-section').eq(i).css({
-          display: 'none'
-        })
-      } else {
-        $('.js-section').eq(i).css({
-          display: 'block'
-        })
-      }
-      if (currentSection > 1) {
-        $('.js-section').eq(i).css({
-          backgroundColor: bg,
-          transitionDelay: (i-currentSection)/10 + 's'
-        })
-      }
-    }
-    $('.js-section').removeClass('active')
-    $('.js-section').eq(currentSection).css({
-      backgroundColor: ''
-    }).addClass('active')
+    if (!accessible) {
+      requestAnimationFrame(function(){
+        var scale = Math.pow(aspect,rotation/90);
+        currentSection = Math.min(21,Math.max(-16,Math.floor((rotation-30)/-90)));
+        if (currentSection < 20 && currentSection > -15) {
+          $('.js-spiral').css({
+            transform: 'rotate(' + rotation + 'deg) scale(' + scale + ')',
+          })
+          $('.js-dot').removeClass('op-100');
+          $('.js-dot').eq(currentSection).addClass('op-100')
+          var currentNum = currentSection + 1;
+          if (currentNum < 1 || currentNum > 13) {
+            currentNum = "LOST";
+          }
+          $('.js-current').text(currentNum);
+          currentSectionName = $('.js-section').eq(currentSection).data('project')
+          var bg = bgColorsArray[currentSection-1]
+          var color = colorsArray[currentSection-1]
+          BODY.css({
+            backgroundColor: color,
+            color: bg
+          })
+          $('.js-close-bg').css({
+            fill: color,
+          })
+          $('.js-close-lines').css({
+            fill: bg,
+          })
+          $('.js-eye-pupil').css({
+            fill: bg
+          })
+          $('.js-eye-retina').css({
+            fill: color
+          })
+          $('.js-eye-lid').css({
+            fill: color
+          })
+          $('.js-section-title-word span').css({
+            transitionDelay: 0,
+            transition: 0
+          })
+          $('.js-gallery-image').css({
+            background: color
+          })
+          $('.js-dots').css({
+            color: color
+          })
+          $('.js-section').css({
+            color: color
+          })
+          $('.js-view-icon').css({
+            background: color,
+            color: bg
+          })
+          $('.js-view-icon svg rect').css({
+            stroke: bg
+          })
+          $('.js-view-icon svg line').css({
+            stroke: bg
+          })
+          $('.js-view-icon svg polyline').css({
+            stroke: bg
+          })
+          $('.js-nav').css({
+            color: color
+          })
+          $('.js-nav svg path').css({
+            fill: color
+          })
+          for (var i=0;i<$('.js-section').length;i++) {
+            if (i - currentSection < -1) {
+              $('.js-section').eq(i).css({
+                visibility: 'hidden'
+              })
+            } else {
+              $('.js-section').eq(i).css({
+                visibility: 'visible'
+              })
+            }
+            $('.js-section').eq(i).css({
+              background: bg,
+              transitionDelay: (i-currentSection)/20 + 's'
+            })
+          }
+          $('.js-section').removeClass('active')
+          $('.js-section').eq(currentSection).css({
+            backgroundColor: ''
+          }).addClass('active')
 
-    $('.js-spiral-line').css({
-      display: 'block'
+        }
+        if (currentSection < 0) {
+          spiraling = 'white';
+        } else if (currentSection > $('.js-section').length) {
+          spiraling = 'black';
+          $('.js-spiral').css({
+            pointerEvents: 'none'
+          })
+        } else {
+          spiraling = false;
+          $('.js-spiral').css({
+            pointerEvents: 'auto'
+          })
+        }
+      })
+    }
+  }
+  function trimRotation() {
+    return Math.max(-1500, Math.min(1200, rotation))
+  }
+  function moveCloseIcon(e) {
+    $('.js-close-icon').css({
+      transform: 'translate3d(' + Math.floor(e.pageX - 24) + 'px,' + Math.min($('.js-gallery-description-box').offset().top - 30 - BODY.scrollTop(),Math.floor(e.pageY - BODY.scrollTop() - 24)) + 'px,0)',
+      right: 'auto'
     })
   }
 
-  function moveCloseIcon(e) {
-    if (BODY.hasClass('is-gallery')) {
-      $('.js-close-icon').css({
-        transform: 'translate3d(' + Math.floor(e.pageX - 24) + 'px,' + Math.floor(e.pageY - 24) + 'px,0)'
-      })
+  function mousemoveHandler(e) {
+    if (!accessible) {
+      if (BODY.hasClass('is-gallery') && !smallScreen) {
+        moveCloseIcon(e);
+      } else {
+        var currentSectionEl = $('.js-section.active');
+        var retina = $('.js-eye-retina', currentSectionEl)
+        var pupil = $('.js-eye-pupil', currentSectionEl)
+        var highlight = $('.js-eye-highlight', currentSectionEl)
+        var lid = $('.js-eye-lid', currentSectionEl)
+        if (retina.offset()) {
+          retina.css({
+            transform: 'translate3d(' + (1-e.pageX/retina.offset().left)*-6 + 'px,' + (1-e.pageY/retina.offset().top)*-6 + 'px,0)'
+          })
+          pupil.css({
+            transform: 'translate3d(' + (1-e.pageX/retina.offset().left)*-7 + 'px,' + (1-e.pageY/retina.offset().top)*-7 + 'px,0)'
+          })
+          highlight.css({
+            transform: 'translate3d(' + (1-e.pageX/retina.offset().left)*-2 + 'px,' + (1-e.pageY/retina.offset().top)*-2 + 'px,0)'
+          })
+          lid.css({
+            transform: 'scaleY(' +  Math.abs(1+Math.max(0,(1-Math.abs(e.pageY/retina.offset().top))*.2)) + ')'
+          })
+        }
+      }
     }
   }
 
   function resizeHandler () { // Set the size of images and preload them
     _winW = window.innerWidth;
     _winH = window.innerHeight;
-    buildSpiral()
+    if (_winW < 960) {
+      smallScreen = true;
+    } else {
+      smallScreen = false;
+    }
+    if (!accessible) {
+      buildSpiral()
+    }
   }
   function buildSpiral() {
     spiralOrigin = Math.floor(_winW * axis) + 'px ' + Math.floor(_winW * aspect * axis) +'px';
@@ -198,67 +389,64 @@ $(function() {
       h = _winW;
     }
     var translate = '';
-    if (safari) {
+    if (safari || firefox) {
       translate = 'translate3d(0,0,0)'
+      $('svg').css({
+        transform: 'translate3d(0,0,0.0001px) scale(1)',
+        backfaceVisiblity: 'hidden',
+      })
+      $('.js-view-icon').css({
+        transform: 'translate3d(0,0,0)',
+        backfaceVisiblity: 'hidden'
+      })
     }
     $('.js-spiral').css({
       transformOrigin: spiralOrigin,
       backfaceVisiblity: 'hidden'
     })
+    $('.js-total').text($('.js-section').length)
+
     $('.js-section').each(function(i){
+      if ($('.js-dot').length < $('.js-section').length) {
+        var dot = $('.js-dot').eq(0).clone()
+        $('.js-dots').append(dot)
+      }
       var myRot = Math.floor(90*i);
-      var scale = toFixed(Math.pow(aspect, i));
-      console.log(scale)
+      var scale = Math.pow(aspect, i);
       $(this).css({
         width: w,
         height: h,
         transformOrigin: sectionOrigin,
         backfaceVisiblity: 'hidden',
       })
-      document.querySelectorAll('.js-section')[i].style.transform = 'rotate(' + myRot + 'deg) scale(' + toFixed(Math.pow(aspect, i)) + ') ' + translate
+      document.querySelectorAll('.js-section')[i].style.transform = 'rotate(' + myRot + 'deg) scale(' + Math.pow(aspect, i) + ') ' + translate
       if (i > 0 && bgColorsArray.length < $('.js-section').length) {
-        var bg = $(this).css('background')
-        bg = bg.substr(0, bg.indexOf(')'))
+        var bg = $(this).css('backgroundColor')
+        bg = bg.substr(0, bg.indexOf(')') + 1)
         var color = $(this).css('color')
         colorsArray.push(color);
         bgColorsArray.push(bg);
         $(this).css({
-          backgroundColor: '#000'
+          backgroundColor: bgColorsArray[0],
+          color: colorsArray[0]
         })
       }
     })
-    function toFixed(x) {
-      if (Math.abs(x) < 1.0) {
-        var e = parseInt(x.toString().split('e-')[1]);
-        if (e) {
-            x *= Math.pow(10,e-1);
-            x = '0.' + (new Array(e)).join('0') + x.toString().substring(2);
-        }
-      } else {
-        var e = parseInt(x.toString().split('+')[1]);
-        if (e > 20) {
-            e -= 20;
-            x /= Math.pow(10,e);
-            x += (new Array(e+1)).join('0');
-        }
-      }
-      return x;
-    }
     scrollHandler();
+    BODY.addClass('is-in')
   }
 
   function startScrollTimeout () {
     clearTimeout(showLineTimeout)
-    showLineTimeout = setTimeout(function(){
-      // $('.js-spiral-line').css({
-      //   display: 'none'
-      // })
-      console.log(rotation%90)
-      if (rotation%90 < -80 || rotation%90 > -20) {
+    if (currentSection > -1 && currentSection < $('.js-section').length) {
+      showLineTimeout = setTimeout(function(){
+        // $('.js-spiral-line').css({
+        //   display: 'none'
+        // })
         cancelAnimationFrame(animRAF);
-        animateScroll(currentSection * -90,rotation,.05);
-      }
-    },500);
+        animateScroll(currentSection * -90,rotation,.15);
+      },200);
+    }
   }
 
 })
